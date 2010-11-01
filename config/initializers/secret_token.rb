f5880a7bc586fd66c6c6c63e0ef5ab61211bde23
Davide Targa <davide.targa@gmail.com>
2010-11-01 16:27:38 +0100

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OrderManager::Application.config.secret_token = 'ae40cfe974518b83542716f1a945ed3a70cfb4b95623b358f3b7ec34252401e68c6b65d6aa9bcf5c0e9a839543153ca9e163352d10434fba67dc3c41b7cc6dd6'
