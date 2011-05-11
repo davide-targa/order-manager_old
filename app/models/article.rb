d927276d73f7bac9ab57e80405a3422d8f8d21ed
Davide Targa <davide.targa@gmail.com>
2011-05-11 14:03:36 +0200

    Added validation to ensure barcode uniqueness inside the order.

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,4 +6,23 @@ class Article < ActiveRecord::Base
   validates :barcode, :presence => true
   validates :size, :uniqueness => { :scope => :model_id }, :presence => true, :inclusion => { :in => CONSTANTS[:valid_numbers] }
 
+  validate :barcode_uniqueness_inside_order
+
+  # Validazione per verificare che il codice a barre
+  # sia unico all'interno dell'ordine.
+  #
+  def barcode_uniqueness_inside_order
+    if self.barcode and self.size
+      order_models = self.model.order.models
+      order_models.each do |model|
+        model.articles.each do |article|
+          if article.barcode == self.barcode
+            errors[:barcode] << "deve essere unico nell'ordine"
+            return false
+          end
+        end
+      end
+    end
+  end
+  
 end
